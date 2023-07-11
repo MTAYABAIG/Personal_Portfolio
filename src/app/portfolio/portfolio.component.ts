01e5c9dec192cabe1d4ad3e9c0f72672c1174a1e
Muhammad Tayyab Baig <116713172+MTAYABAIG@users.noreply.github.com>
2023-07-11 19:13:05 +0500

    gg

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  projectArray = [
+    {
+      sourceLink: './assets/images/counter.png',
+      title: 'UI Follower Counter',
+      desp: "I design and develop Follower Counter with the help of Html,Css And Js",
+      previewLink : "https://mtayabaig.github.io/15-Counter/"
+    },
+    {
+      sourceLink: './assets/images/waterapp.png',
+      title: 'Water Drinking App',
+      desp: "I created a water app using html,css and js",
+      previewLink : "https://mtayabaig.github.io/water-app/"
+    },
+    {
+      sourceLink: './assets/images/waterapp.png',
+      title: 'Water Drinking App',
+      desp: "I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores",
+      previewLink : "https://mtayabaig.github.io/water-app/"
+    },
+  ];
+
 }
